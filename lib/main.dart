b70003db1ee7d11ad05a006b383e0d04965bf7bb
vudoan2016 <vudoant@gmail.com>
2020-09-07 21:26:57 -0700

    Retrieved words with definitions from the server.

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-void main() {
-  runApp(MyApp());
-}
+class Vocabulary {
+  final String word;
+  final String type;
+  final String definition;
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+  Vocabulary({this.word, this.type, this.definition});
+
+  String toString() {
+    return "Word: $word, price: $type, today gain: $definition";
+  }
+
+  factory Vocabulary.fromJson(Map<String, dynamic> json) {
+    return Vocabulary(
+      word: json['Word'],
+      type: json['Type'],
+      definition: json['Definition'],
     );
   }
 }
+
+Future<List<Vocabulary>> fetchVocabulary() async {
+  final response =
+  await http.get('http://192.168.0.15:8081/', headers: {"Accept": "application/json"});
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    var list = json.decode(response.body) as List;
+    List<Vocabulary> vocabularys = list.map((e) => Vocabulary.fromJson(e)).toList();
+    return vocabularys;
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to fetch vocabulary');
+  }
+}
+
+void main() => runApp(MyHomePage());
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -50,18 +60,26 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  Future<List<Vocabulary>> futureVocabulary;
+  int _index = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  PageController _controller = PageController(
+    initialPage: 0,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    futureVocabulary = fetchVocabulary();
   }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _handlePageChange(int index) => setState(() => _index = index);
 
   @override
   Widget build(BuildContext context) {
@@ -71,47 +89,40 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return MaterialApp(
+        title: 'iSpell',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        home: Scaffold(
+          appBar: AppBar(centerTitle: true, title: new Text(_index.toString(),)),
+          body: FutureBuilder<List<Vocabulary>>(
+              future: futureVocabulary,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  Vocabulary v = snapshot.data[_index];
+                  return PageView.builder(
+                    itemBuilder: (context, position) {
+                      return Container(
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text((v.word + ' (' + v.type + ')'),
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
+                              Text(('Definition: ' + v.definition),
+                                  style: TextStyle(fontSize: 20))]),
+                      );
+                    },
+                    itemCount: snapshot.data.length,
+                    controller: _controller,
+                    onPageChanged: _handlePageChange,
+                  );}
+                // By default, show a loading spinner.
+                return CircularProgressIndicator();
+              })
+        )
     );
   }
 }
